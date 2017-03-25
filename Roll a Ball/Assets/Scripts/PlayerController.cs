4ae5715b53906ea86238f90bf0c8600f5364f7cd
Craig <cbodonnell@comcast.net>
2017-03-25 17:09:17 -0400

    Added PlayerController Script
    PlayerController takes user input and applies a force on the object in
    the direction of the input.
    
    Added an public speed variable to control the magnitude of the force
    that is applied by the user input.

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Gets rigidbody component from owner of PlayerController
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	// Update is called just before a physics calculation is performed
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+		
+}
